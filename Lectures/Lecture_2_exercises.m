89cfeac8639de708c439b6f54bcdcd35292440b9
Andris Vaivads <andris.vaivads@gmail.com>
2015-08-02 09:02:55 +0200

    Lecture 2: artificial boundary

--- Lectures/Lecture_2_exercises.m
+++ Lectures/Lecture_2_exercises.m
@@ -116,3 +116,26 @@ irf_legend(h(1),{'FNew','Fnew=F*2+2'},[0.02 0.98])
 Hours=TSeries(T,(T-T.start)/3600); % hours from the beginning of the time interval
 irf_timeaxis(h(2),Hours,{'hours'})
 irf_timeaxis(h(end),'nodate');
+
+%% %% Example XX time interval
+% Lets generate 5samples/s time series during 1h after 2002-03-04 09:30 UTC,
+% including artificial boundary in the middle of the interval.
+
+T   = EpochTT('2002-03-04T09:30:00Z'):.2...
+     :EpochTT('2002-03-04T10:30:00Z');      % define time line as EpochTT object
+t   = T - T.start;                % define relative time in s from start
+Bx  = 5*tanh((t-1800)/60);      % define Bx jump 
+By  = -3*tanh((t-1800)/10);      % define Bx jump 
+Bz  = t*0; 
+TS1 = irf.ts_vec_xyz(T,[Bx By Bz]);                   % define scalar TSeries object
+TS1.units = 'nT';
+TS1.userData.LABLAXIS = 'B';
+
+tInt = irf.tint('2002-03-04T09:40:00Z/2002-03-04T09:50:00Z');
+
+TS2 = TS1.tlim(tInt);
+
+h   = irf_plot(1,'newfigure');			        % initialize figure
+irf_plot(h,TS1);						                % plot times series  
+irf_minvar_gui(TS1);
+
