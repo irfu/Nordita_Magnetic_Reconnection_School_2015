3f8f2a8c68edf9e7f51bce7bae09b47c1912a6eb
Andris Vaivads <andris.vaivads@gmail.com>
2015-08-02 15:09:16 +0200

    Lecture2: plot real data

--- Lectures/Lecture2/Lecture_2_exercises.m
+++ Lectures/Lecture2/Lecture_2_exercises.m
@@ -68,49 +68,8 @@ irf_minvar_gui(TS1);                  % run minimum variance analysis on time se
 tInt = irf.tint('2001-01-26T10:30:00Z/2001-01-26T11:00:00Z');
 %caa_download(tInt,'C1_CP_FGM_SPIN');
 caa_load C1_CP_FGM_SPIN
-
-
-%% Example  2 Plot multicomponent data
-% Generate data with two components and plot in the same figure.
-% Add legend text in lower left corner
-% As you notice irfu-matlab interprets some common names for variables, 
-% i.e. B2 is assumed to be magnetic field measurement by Cluster 2
-
-y = exp(0.001*t).*cos(2*pi*t/180);	% z(t)=exp(0.001(t-to))*cos(t)
-F = TSeries(T,[x y]);							          % B2 has two components, x & y
-irf_plot(h,F)					          % plot in the same axis
-irf_legend({'X','Y'},[0.02 0.02])	% add legend text with the same colors as lines
-
-%% Example 3 Work with data, zoom in plots. 
-% - Generate second data set that is a function of first.
-%     Fnew = F*1.2 + 2	 
-% - Plot both data sets in separate panels.
-% - Zoom in to smaller 30min time interval .
-
-Fnew = F*2 + 2;
-
-h  = irf_plot(2,'newfigure');		% empty figure with 2 panels
-irf_plot(h(1),F);
-ylabel(h(1),'F');
-irf_legend(h(1),{'X','Y'},[0.02 0.98],'fontsize',20)
-irf_plot(h(2),Fnew);
-ylabel(h(2),'F_{new} = F * 2 + 2');
-
-tint_zoom = irf_time([2008 03 01 10 20 00]) + [0 1800]; % 10 min interval
-irf_zoom(h,'x',tint_zoom);
-irf_zoom(h,'y');
-
-%% Example  4 Compare two data  
-% Compare component-wise two datasets.
-% Add one more label row with hours from the beginning of time interval
-
-h=irf_plot({F,Fnew},'comp');
-ylabel(h(1),'F_X');
-title(h(1),T.start.utc('yyyy-mm-dd'));
-ylabel(h(2),'F_Y');
-irf_legend(h(1),{'FNew','Fnew=F*2+2'},[0.02 0.98])
-
-Hours=TSeries(T,(T-T.start)/3600); % hours from the beginning of the time interval
-irf_timeaxis(h(2),Hours,{'hours'})
-irf_timeaxis(h(end),'nodate');
+B1 = irf_get_data('B_vec_xyz_gse__C1_CP_FGM_SPIN','caa','ts');
+irf_plot(1,'newfigure');
+irf_plot(B1);
+irf_minvar_gui(B1)
 
