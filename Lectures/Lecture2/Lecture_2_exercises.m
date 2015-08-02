4975ba2a2c2916d2da911216dd91099db2401737
Andris Vaivads <andris.vaivads@gmail.com>
2015-08-02 22:31:16 +0200

    Lecture2: added getting TSeries from real data

--- Lectures/Lecture2/Lecture_2_exercises.m
+++ Lectures/Lecture2/Lecture_2_exercises.m
@@ -6,6 +6,8 @@
 % Minimum Variance Analysis
 % De Hofmann-Teller frame
 % 
+% As real data we use Paschmann 2005 paper
+
 
 %% Example 1 ideal boundary, B change in one component
 % Lets generate 5samples/s time series during 1h after 2002-03-04 09:30 UTC,
@@ -65,11 +67,15 @@ irf_minvar_gui(TS1);                  % run minimum variance analysis on time se
 %% Example XX, magnetopause crossing Rosenqvist2008
 % http://www.cluster.rl.ac.uk/csdsweb-cgi/csdsweb_pick?P_TYPE=P1&YEAR=2001&MONTH=Jan&DAY=26&SUB_PLOT=S02
 
-tInt = irf.tint('2001-01-26T10:30:00Z/2001-01-26T11:00:00Z');
+% tInt = irf.tint('2001-01-26T10:30:00Z/2001-01-26T11:00:00Z');
+tInt = irf.tint('2001-07-05T04:30:00Z/2001-07-05T06:30:00Z');
+
 %caa_download(tInt,'C1_CP_FGM_SPIN');
+%caa_download(tInt,'C1_CP_EFW_L3_E3D_GSE');
 caa_load C1_CP_FGM_SPIN
 B1 = irf_get_data('B_vec_xyz_gse__C1_CP_FGM_SPIN','caa','ts');
 irf_plot(1,'newfigure');
 irf_plot(B1);
 irf_minvar_gui(B1)
 
+%% De Hofmann - Teller frame
